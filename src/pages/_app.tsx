032d703c2f50b4c033092021d878ffff0d683830
computerextra <service@computer-extra.de>
2024-03-12 10:11:33 +0100

    shadcn config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
-import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
-
-import { api } from "@/utils/api";
-
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import { api } from "@/utils/api";
+import { type AppType } from "next/app";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable,
+      )}
+    >
       <Component {...pageProps} />
     </main>
   );
