385d4822d579efc622849aeecaf9141186f0294b
computerextra <service@computer-extra.de>
2024-03-14 17:38:50 +0100

    Update env.js

--- src/env.js
+++ src/env.js
@@ -20,6 +20,8 @@ export const env = createEnv({
     SMTP_PORT: z.number().int(),
     SMTP_USER: z.string(),
     SMTP_PASS: z.string(),
+    UPLOADTHING_SECRET: z.string(),
+    UPLOADTHING_APP_ID: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -48,6 +50,8 @@ export const env = createEnv({
       : undefined,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
