b670f063fbb3e285f1b9185edae2a86bd5bc3e5e
computerextra <service@computer-extra.de>
2024-03-15 11:30:49 +0100

    Archive fertig

--- src/server/api/routers/Archive.ts
+++ src/server/api/routers/Archive.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
+import { env } from "@/env";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { readFile } from "fs/promises";
+import path from "path";
 
 export const ArchiveRouter = createTRPCRouter({
-  get: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+  get: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
     return ctx.db.pdfs.findMany({
       where: {
         OR: [
@@ -19,6 +22,25 @@ export const ArchiveRouter = createTRPCRouter({
           },
         ],
       },
+      select: {
+        id: true,
+        title: true,
+      },
     });
   }),
+  download: publicProcedure
+    .input(z.number().int())
+    .mutation(async ({ ctx, input }) => {
+      const Filename = await ctx.db.pdfs.findUnique({
+        where: { id: input },
+        select: { title: true },
+      });
+
+      if (Filename == null) return null;
+
+      const filePath = path.join(env.ARCHIVE_PATH, Filename.title);
+      const data = await readFile(filePath);
+      const dataString = data.toString("base64");
+      return dataString;
+    }),
 });
