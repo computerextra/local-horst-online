162257fe5b1a4be673092127d91f1f0f5500bedd
computerextra <service@computer-extra.de>
2024-04-23 09:33:51 +0200

    alles weg

--- src/pages/Telefon/Lieferanten/Ansprechpartner/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { Anschprechpartner } from "@prisma/client";
-import { columns } from "./_components/columns";
-import { DataTable } from "./_components/data-table";
-
-export default function APTable({ data }: { data: Anschprechpartner[] }) {
-  return <DataTable columns={columns} data={data} />;
-}
