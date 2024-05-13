0b779004b2595ace4633262f68873fd9ba5a470c
computerextra <service@computer-extra.de>
2024-05-13 11:17:37 +0200

    Update index.tsx

--- src/pages/Geburtstag/index.tsx
+++ src/pages/Geburtstag/index.tsx
@@ -9,39 +9,53 @@ export default function Geburtstage() {
   const Mitarbeiter = api.Mitarbeiter.getAll.useQuery();
 
   const [heute, setHeute] = useState<undefined | Mitarbeiter[]>(undefined);
-  const [andere, setAndere] = useState<undefined | Mitarbeiter[]>(undefined);
+  const [zukunft, setZukunft] = useState<undefined | Mitarbeiter[]>(undefined);
+  const [vergangenheit, setVergangenheit] = useState<undefined | Mitarbeiter[]>(
+    undefined
+  );
 
   useEffect(() => {
     if (Mitarbeiter.data == null) return;
 
-    const today = new Date().toLocaleDateString();
-    const jahr = new Date().getFullYear();
-
-    const andere: Mitarbeiter[] = [];
+    const zukunft: Mitarbeiter[] = [];
     const heute: Mitarbeiter[] = [];
+    const vergangenheit: Mitarbeiter[] = [];
+
+    const sortByBirthday = (arr: Mitarbeiter[]): Mitarbeiter[] => {
+      return arr.sort(
+        (a, b) =>
+          new Date(
+            new Date().getFullYear(),
+            a.Geburtstag!.getMonth(),
+            a.Geburtstag!.getDate()
+          ).getTime() -
+          new Date(
+            new Date().getFullYear(),
+            b.Geburtstag!.getMonth(),
+            b.Geburtstag!.getDate()
+          ).getTime()
+      );
+    };
 
     Mitarbeiter.data.forEach((mitarbeiter) => {
       if (mitarbeiter.Geburtstag) {
-        const day = mitarbeiter.Geburtstag.getDate();
-        const month = mitarbeiter.Geburtstag.getMonth();
-        const bday = new Date(jahr, month, day).toLocaleDateString();
-        console.log(mitarbeiter.Name, bday, today, mitarbeiter.Geburtstag);
-        if (bday == today) {
+        const bday = new Date(
+          new Date().getFullYear(),
+          mitarbeiter.Geburtstag.getMonth(),
+          mitarbeiter.Geburtstag.getDate()
+        );
+        if (bday.toLocaleDateString() == new Date().toLocaleDateString())
           heute.push(mitarbeiter);
-        } else {
-          andere.push(mitarbeiter);
-        }
+        else if (bday < new Date()) vergangenheit.push(mitarbeiter);
+        else zukunft.push(mitarbeiter);
       }
     });
 
-    if (heute.length > 0) heute.sort((a, b) => a.Name.localeCompare(b.Name));
-    if (andere.length > 0) andere.sort((a, b) => a.Name.localeCompare(b.Name));
-
-    if (heute.length > 0) setHeute(heute);
-    else setHeute(undefined);
-
-    if (andere.length > 0) setAndere(andere);
-    else setAndere(undefined);
+    setHeute(heute.length > 0 ? sortByBirthday(heute) : undefined);
+    setVergangenheit(
+      vergangenheit.length > 0 ? sortByBirthday(vergangenheit) : undefined
+    );
+    setZukunft(zukunft.length > 0 ? sortByBirthday(zukunft) : undefined);
   }, [Mitarbeiter.data]);
 
   return (
@@ -66,11 +80,29 @@ export default function Geburtstage() {
             </ul>
           </div>
         )}
-        {andere && (
+        {vergangenheit && (
           <>
-            <h3>Aktuelle Liste:</h3>
+            <h3>Vergangene Geburtstage:</h3>
             <ul>
-              {andere.map((mitarbeiter) => (
+              {vergangenheit.map((mitarbeiter) => (
+                <li key={mitarbeiter.id}>
+                  {mitarbeiter.Name} hatte am{" "}
+                  <span className="fw-bold">
+                    {mitarbeiter.Geburtstag!.getDate()}.
+                    {mitarbeiter.Geburtstag!.getMonth() + 1}
+                  </span>{" "}
+                  Geburtstag
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {zukunft && (
+          <>
+            <h3>Zukünftige Geburtstage:</h3>
+            <ul>
+              {zukunft.map((mitarbeiter) => (
                 <li key={mitarbeiter.id}>
                   {mitarbeiter.Name} hat am{" "}
                   <span className="fw-bold">
